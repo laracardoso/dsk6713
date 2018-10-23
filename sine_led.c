c56a5aa37f62e1178b40e78e1511d56e3ecc6d66
Priscila Santos <aosmeuseus@gmail.com>
2018-10-23 15:27:45 -0300

    test shine_led

--- sine_led.c
+++ sine_led.c
@@ -14,6 +14,7 @@
 #define DSK6713_AIC23_INPUT_MIC 0x0015
 #define DSK6713_AIC23_INPUT_LINE 0x0011
 #define LOOPLENGTH 8
+#define BUF_SIZE 16000           // this sets length of delay
 
 void comm_poll();
 void output_left_sample(short out_data);
@@ -25,6 +26,9 @@ Uint16 inputsource = DSK6713_AIC23_INPUT_LINE;  //select input
 short loopindex = 0; //table index
 short gain = 2;                      //gain factor
 short sine_table[LOOPLENGTH]={0,707,1000,707,0,-707,-1000,-707}; //sine values
+short buffer[BUF_SIZE];         // storage for previous samples
+short input,output,delayed;
+int i = 0;
 
 void main(){
 
@@ -37,11 +41,22 @@ void main(){
     if(DSK6713_DIP_get(0)==0)         //=0 if DIP switch #0 pressed
     {
       DSK6713_LED_on(0);               //turn LED #0 ON
-      output_left_sample(sine_table[loopindex++]*gain); //output sample
-      output_right_sample(sine_table[loopindex++]*gain);
+	  input = sine_table[loopindex++];  // read new input sample from ADC
+	  delayed = buffer[i];          // read delayed value from buffer
+	  output = input*gain + delayed;     // output sum of input and delayed values
+	  output_left_sample(output);
+	  output_right_sample(output);
+	  buffer[i] = input*gain + delayed*0.8;
+	  i++;
+	  loopindex++;
+	  
+	  if(++i >= BUF_SIZE) {
+		i=0;
+		buffer[i] = 0;
+	  }
 
       if (loopindex >= LOOPLENGTH) {
-          loopindex = 0; //reset table index
+		loopindex = 0; //reset table index
       }
     }
     else DSK6713_LED_off(0);          //turn LED off if not pressed
